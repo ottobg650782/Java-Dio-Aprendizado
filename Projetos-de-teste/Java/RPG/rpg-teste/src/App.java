8783d5e2b75ba6a0bf7ac9e05ea97d2ccadb5626
ottobg650782 <ottobg650782@gmail.com>
2022-12-27 15:09:18 -0300

    Movi os arquivos para uma pasta dedicada

---
+++ Projetos-de-teste/Java/RPG/rpg-teste/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int primeiroNumero = 1893;
+        int segundoNumero = 23;
+        float divisao = (primeiroNumero / segundoNumero);
+
+        System.out.println(divisao);
+        System.out.println(0 != 1);
+    }
+}
