f4b15871b76e76e9612dbcbd470e88cffee31468
ottobg650782 <ottobg650782@gmail.com>
2022-12-29 11:36:14 -0300

    Adicionei um switch

--- Exercícios-de-exemplo/exercicio-exemplo-condicional/logica-condicional-controle-de-fluxoi/src/App.java
+++ Exercícios-de-exemplo/exercicio-exemplo-condicional/logica-condicional-controle-de-fluxoi/src/App.java
@@ -1,5 +1,8 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
+        microrpg("sul");
+
         boolean b1 = true;
         boolean b2 = false;
         boolean b3 = true;
@@ -13,5 +16,27 @@ public class App {
         System.out.println(b4 ^ b1);
         System.out.println(!b1);
         System.out.println(!b2);
+    }
+
+    private static void microrpg(String direcao)
+
+    {
+        switch (direcao) {
+            case "norte":
+                System.out.println("Você foi para o norte, existe uma bela cadeia de montanhas por lá.");
+                break;
+            case "sul":
+                System.out.println("Ao sul, você encontra um vasto deserto, uma imensidão de areia.");
+                break;
+            case "leste":
+                System.out.println("Seguindo pelo leste, você chega a um porto deserto.");
+                break;
+            case "oeste":
+                System.out.println("Indo para o oeste, você chega a uma pequena vila, onde as pessoas são felizes.");
+                break;
+            default:
+                System.out.println("Argumento inválido. Utilize apenas norte // sul // leste // oeste");
+                break;
+        }
     }
 }
