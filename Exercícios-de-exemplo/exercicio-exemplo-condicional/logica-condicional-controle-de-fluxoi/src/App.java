542b87b42b09b900947bf02a174210e81a0fdde2
ottobg650782 <ottobg650782@gmail.com>
2022-12-29 11:04:11 -0300

    Exercício adicionado

---
+++ Exercícios-de-exemplo/exercicio-exemplo-condicional/logica-condicional-controle-de-fluxoi/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        boolean b1 = true;
+        boolean b2 = false;
+        boolean b3 = true;
+        boolean b4 = false;
+
+        System.out.println(b1 && b2);
+        System.out.println(b1 && b3);
+        System.out.println(b2 || b3);
+        System.out.println(b2 || b4);
+        System.out.println(b1 ^ b3);
+        System.out.println(b4 ^ b1);
+        System.out.println(!b1);
+        System.out.println(!b2);
+    }
+}
